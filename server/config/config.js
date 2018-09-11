98319b46f3047a2c89095310b96f1e28a905068c
Jose Ramon Parreño Bonillo <jose.parreno.bonillo@accenture.com>
2018-09-11 16:05:54 +0200

    Variable de entorno mongodb mlab

--- server/config/config.js
+++ server/config/config.js
@@ -17,10 +17,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=========================
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/CafeDB';
-// } else {
-urlDB = 'mongodb://test:123456a@ds251902.mlab.com:51902/cafedb';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/CafeDB';
+} else {
+    urlDB = process.env.MONGO_URI_DB;
+}
 
 process.env.URLDB = urlDB;
